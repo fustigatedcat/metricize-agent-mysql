d92f5ad0134a788741c66abd0f6b0051e167ae17
Joshua Backfield <suroot.chmod777@gmail.com>
2015-03-14 18:18:14 -0400

    Finished getting basic handling of mysql working.

--- src/main/scala/com/fustigatedcat/metricize/agent/AgentWorker.scala
+++ src/main/scala/com/fustigatedcat/metricize/agent/AgentWorker.scala
@@ -1,13 +1,8 @@
 package com.fustigatedcat.metricize.agent
 
-import java.sql.{ResultSet, ResultSetMetaData, DriverManager, Connection}
-
-import akka.actor.Actor
-import com.fustigatedcat.metricize.agent.intf.AgentWorkerInterface
+import com.fustigatedcat.metricize.agent.intf.{AgentResponse, AgentWorkerInterface}
+import com.fustigatedcat.metricize.agent.mysql.MYSQLWorker
 import com.typesafe.config.Config
-import org.json4s.JsonAST.{JString, JObject, JArray, JValue}
-import org.json4s.native.{prettyJson, renderJValue}
-import org.json4s.JsonDSL._
 
 class AgentWorker(config : Config) extends AgentWorkerInterface {
 
@@ -21,27 +16,8 @@ class AgentWorker(config : Config) extends AgentWorkerInterface {
 
   val jdbc = s"jdbc:mysql://$fqdn:$port/metricize"
 
-  Class.forName("com.mysql.jdbc.Driver")
-
-  def handleResultSet(rs : ResultSet, rsmd : ResultSetMetaData, out : List[JObject] = List()) : JArray = rs.next() match {
-    case true => {
-      val arr = JObject((for(col <- 0 until rsmd.getColumnCount) yield {
-        rsmd.getColumnName(col + 1) -> (rs.getString(col + 1) : JValue)
-      }).toList)
-      handleResultSet(
-        rs,
-        rsmd,
-        arr :: out
-      )
-    }
-    case _ => out.reverse
-  }
-
-  def process(): Unit = {
-    val conn = DriverManager.getConnection(jdbc, username, password)
-    val stmt = conn.createStatement
-    val rs = stmt.executeQuery("SELECT * FROM Customer")
-    println(prettyJson(renderJValue(handleResultSet(rs, rs.getMetaData))))
+  def process(): AgentResponse = {
+    MYSQLWorker.executeMysqlWorker(jdbc, username, password, "SELECT * FROM Customer")
   }
 
 }
---
+++ src/main/scala/com/fustigatedcat/metricize/agent/mysql/MYSQLWorker.scala
@@ -0,0 +1,44 @@
+package com.fustigatedcat.metricize.agent.mysql
+
+import java.sql.{DriverManager, ResultSetMetaData, ResultSet}
+
+import com.fustigatedcat.metricize.agent.intf.{AgentResponseFailure, AgentResponseSuccess}
+import org.json4s.JsonAST.{JValue, JArray, JObject}
+import org.json4s.JsonDSL._
+import org.json4s.native._
+
+object MYSQLWorker {
+
+  Class.forName("com.mysql.jdbc.Driver")
+
+  def handleResultSet(rs : ResultSet, rsmd : ResultSetMetaData, out : List[JObject] = List()) : JArray = rs.next() match {
+    case true => {
+      val arr = JObject((for(col <- 0 until rsmd.getColumnCount) yield {
+        rsmd.getColumnName(col + 1) -> (rs.getString(col + 1) : JValue)
+      }).toList)
+      handleResultSet(
+        rs,
+        rsmd,
+        arr :: out
+      )
+    }
+    case _ => out.reverse
+  }
+
+  def executeMysqlWorker(jdbc : String, username : String, password : String, query : String) = {
+    var start = System.currentTimeMillis()
+    try {
+      val conn = DriverManager.getConnection(jdbc, username, password)
+      val stmt = conn.createStatement
+      start = System.currentTimeMillis()
+      val rs = stmt.executeQuery(query)
+      val end = System.currentTimeMillis()
+      val rtn = handleResultSet(rs, rs.getMetaData)
+      conn.close()
+      AgentResponseSuccess(end - start, prettyJson(renderJValue(rtn)))
+    } catch {
+      case err: Throwable => AgentResponseFailure(System.currentTimeMillis() - start, err.getLocalizedMessage)
+    }
+  }
+
+}
